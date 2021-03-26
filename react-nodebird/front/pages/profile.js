69b937a34aed58480da09ca3e90ea144c6285895
Austin.MacbookPro <bellin83@gmail.com>
2021-03-27 07:04:14 +0900

    :recycle: Refactor header

--- react-nodebird/front/pages/profile.js
+++ react-nodebird/front/pages/profile.js
@@ -5,16 +5,7 @@ import AppLayout from "../components/AppLayout";
 const Profile = () => {
   return(
     <>
-      <Head>
-        <title>NodeBird</title>
-        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css' />
-        <script src='https://cndjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.js' />
-      </Head>
-      <AppLayout>
-        <div>
-          내 프로필
-        </div>
-      </AppLayout>
+      <div>내 프로필</div>
     </>
   )
 };
