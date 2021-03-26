f1033d7144c29979e25b7378a1641aea40362373
bellin83 <bellin83@gmail.com>
2021-03-26 11:04:45 +0900

    :seedling: Add profile.js, signup.js

--- react-nodebird/front/pages/index.js
+++ react-nodebird/front/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import Head from 'next/head';
 import AppLayout from "../components/AppLayout";
 import 'antd/dist/antd.css';
@@ -12,7 +11,6 @@ const Home = () => {
         <title>NodeBird</title>
       </Head>
       <AppLayout>
-        <Link href='/about'><a>about</a></Link>
         <div>Hello, Next!</div>
       </AppLayout>
     </>
