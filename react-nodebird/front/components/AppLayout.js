e56ee07c6f9b1fe508d63699868470ad0d7311c8
Austin.MacbookPro <bellin83@gmail.com>
2021-03-28 06:26:43 +0900

    :art: Add signup Form data

--- react-nodebird/front/components/AppLayout.js
+++ react-nodebird/front/components/AppLayout.js
@@ -8,6 +8,7 @@ const dummy = {
   Post: [],
   Followings: [],
   Followers: [],
+  isLoggedIn: false,
 };
 
 const AppLayout = ({ children }) => {
@@ -22,33 +23,40 @@ const AppLayout = ({ children }) => {
       </Menu>
       <Row>
         <Col xs={24} md={6}>
-          <Card
-            actions={[
-              <div key={"twit"}>짹짹<br />{dummy.Post.length}</div>,
-              <div key={"twit"}>팔로잉<br />{dummy.Followings.length}</div>,
-              <div key={"twit"}>팔로워<br />{dummy.Followers.length}</div>
-            ]}
-          >
-            <Card.Meta
-              avatar={<Avatar>{dummy.nickname[0]}</Avatar>}g
-              title={dummy.nickname}
-            />
-          </Card>
-          <Form>
-            {/*<div>*/}
-            {/*  <label htmlFor="user-id">아이디</label>*/}
-            {/*  <br />*/}
-            {/*  <Input name="user-id" value={userId} onChange={onChangeId} required />*/}
-            {/*</div>*/}
-          </Form>
-          <Link href='/signup'><a><Button>회원가입</Button></a></Link>
+          {dummy.isLoggedIn ?
+            <Card
+              actions={[
+                <div key={"twit"}>짹짹<br/>{dummy.Post.length}</div>,
+                <div key={"twit"}>팔로잉<br/>{dummy.Followings.length}</div>,
+                <div key={"twit"}>팔로워<br/>{dummy.Followers.length}</div>
+              ]}
+            >
+              <Card.Meta
+                avatar={<Avatar>{dummy.nickname[0]}</Avatar>} g
+                title={dummy.nickname}
+              />
+            </Card>
+            :
+            <Form>
+              <div>
+                <label htmlFor="user-id">아이디</label>
+                <br/>
+                <Input name="user-id" required/>
+              </div>
+              <div>
+                <label htmlFor="user-password">비밀번호</label>
+                <br/>
+                <Input name="user-password" type="password" required/>
+              </div>
+              <div>
+                <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
+                <Link href='/signup'><a><Button>회원가입</Button></a></Link>
+              </div>
+            </Form>
+          }
         </Col>
-        <Col xs={24} md={12}>
-
-        </Col>
-        <Col xs={24} md={6}>
-
-        </Col>
+        <Col xs={24} md={12}></Col>
+        <Col xs={24} md={6}></Col>
       </Row>
       {children}
     </div>
