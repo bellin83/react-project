c7d9330b4fcc23b864524b924f7c8cbc516a2ea6
Austin.MacbookPro <bellin83@gmail.com>
2021-04-03 07:56:22 +0900

    :art: Connect sagaMiddleware

--- react-nodebird/front/pages/_app.jsx
+++ react-nodebird/front/pages/_app.jsx
@@ -6,6 +6,8 @@ import AppLayout from "../components/AppLayout";
 import {createStore, compose, applyMiddleware} from 'redux';
 import { Provider } from "react-redux";
 import reducer from "../reducers";
+import sagaMiddleware from "../sagas/middleware";
+import rootSaga from "../sagas";
 
 const NodeBird = ({ Component, store }) => {
   return (
@@ -28,11 +30,14 @@ NodeBird.propTypes = {
 };
 
 export default withRedux((initialState, options) => {
-  const middlewares = [];
-  const enhancer = compose(
-    applyMiddleware(...middlewares),
-    !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
-  );
+  const middlewares = [sagaMiddleware];
+  const enhancer = process.env.NODE_ENV === 'production'
+    ? compose(applyMiddleware(...middlewares))
+    : compose(
+      applyMiddleware(...middlewares),
+      !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+    );
   const store = createStore(reducer, initialState, enhancer);
+  sagaMiddleware.run(rootSaga);
   return store;
 })(NodeBird);
