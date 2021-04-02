d946320dbc355246282f0a5e404fe82e209da6c5
Austin.MacbookPro <bellin83@gmail.com>
2021-04-03 07:19:58 +0900

    :art: Add user saga sample logic

--- react-nodebird/front/sagas/index.jsx
+++ react-nodebird/front/sagas/index.jsx
@@ -1,3 +1,10 @@
-export default function* rootSaga () {
+import { all, call } from 'redux-saga/effects';
+import user from './user';
+import post from './post';
 
+export default function* rootSaga () {
+  yield all([
+    call(user),
+    call(post),
+  ]);
 }
