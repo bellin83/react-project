6c211209aac226a8e13747654c4ee110caa9e67b
Austin.MacbookPro <bellin83@gmail.com>
2021-04-05 05:20:55 +0900

    :art: Add user reducer const

--- react-nodebird/front/sagas/user.jsx
+++ react-nodebird/front/sagas/user.jsx
@@ -1,4 +1,4 @@
-import {all, fork, put, takeLatest, call, take} from "redux-saga/effects";
+import {all, fork, put, takeLatest, takeEvery, call, take} from "redux-saga/effects";
 import {LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE} from "../reducers/user";
 
 function loginAPI() {
