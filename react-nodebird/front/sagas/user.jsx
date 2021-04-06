8ba78989ca8b58d83da9b1c0eb6309606f0532b4
Austin.MacbookPro <bellin83@gmail.com>
2021-04-07 05:47:55 +0900

    :art: Add reducer consts

--- react-nodebird/front/sagas/user.jsx
+++ react-nodebird/front/sagas/user.jsx
@@ -1,10 +1,14 @@
 import {
-  all, fork, put, takeLatest, takeEvery, call, take,
+  all, fork, put, takeEvery, call,
 } from 'redux-saga/effects';
-import { LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE } from '../reducers/user';
+import axios from 'axios';
+import {
+  LOG_IN_SUCCESS, LOG_IN_FAILURE, SIGN_UP_REQUEST, LOG_IN_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE,
+} from '../reducers/user';
 
 function loginAPI() {
   // 서버에 요청을 보내는 부분
+  return axios.post('/login');
 }
 
 function* login() {
@@ -22,17 +26,35 @@ function* login() {
 }
 
 function* watchLogin() {
-  while (true) {
-    yield take(LOG_IN);
+  yield takeEvery(LOG_IN_REQUEST, login);
+}
+
+function signUpAPI() {
+  // 서버에 요청을 보내는 부분
+  return axios.post('/login');
+}
+
+function* signUp() {
+  try {
+    yield call(signUpAPI);
+    yield put({ // put은 dispatch와 동일
+      type: SIGN_UP_SUCCESS,
+    });
+  } catch (e) { // loginAPI 실패
+    console.error(e);
     yield put({
-      type: LOG_IN_FAILURE,
+      type: SIGN_UP_FAILURE,
     });
   }
 }
 
+function* watchSignUp() {
+  yield takeEvery(SIGN_UP_REQUEST, signUp);
+}
+
 export default function* userSaga() {
   yield all([
-    watchLogin(),
-    // watchSignup(),
+    fork(watchLogin),
+    fork(watchSignUp),
   ]);
 }
