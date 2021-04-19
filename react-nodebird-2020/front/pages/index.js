d20716b463fd5f5b81ed1ffb04fe7095b6840e41
Austin.MacbookPro <bellin83@gmail.com>
2021-04-20 04:50:57 +0900

    :bento: Fix index.js

--- react-nodebird-2020/front/pages/index.js
+++ react-nodebird-2020/front/pages/index.js
@@ -1,7 +1,14 @@
-const Home = () => {
-  return (
+import React from 'react';
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
+
+const Home = () => (
+  <AppLayout>
+    <Head>
+      <title>NodeBird</title>
+    </Head>
     <div>Hello, Next!</div>
-  );
-}
+  </AppLayout>
+);
 
 export default Home;
