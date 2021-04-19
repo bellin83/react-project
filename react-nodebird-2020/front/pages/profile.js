087cfc68bbce876d204698bf2ef9bc12a45cbf32
Austin.MacbookPro <bellin83@gmail.com>
2021-04-20 04:51:25 +0900

    :bento: Fix profile.js

--- react-nodebird-2020/front/pages/profile.js
+++ react-nodebird-2020/front/pages/profile.js
@@ -1,7 +1,15 @@
-const Home = () => {
-  return (
-    <div>Hello, Next!</div>
-  );
-}
+import React from 'react';
+import Head from 'next/head';
 
-export default Home;
+import AppLayout from '../components/AppLayout';
+
+const Profile = () => (
+  <AppLayout>
+    <Head>
+      <title>NodeBird</title>
+    </Head>
+    <div>내 프로필</div>
+  </AppLayout>
+);
+
+export default Profile;
