4a65dcc2a30e5ffd89621ab46b6d2739e7d85cfd
bellin83 <bellin83@gmail.com>
2021-04-20 09:05:03 +0900

    :bento: Add chapter2 files

--- react-nodebird-2020/front/components/AppLayout.js
+++ react-nodebird-2020/front/components/AppLayout.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
-import { Menu, Input, Button } from 'antd';
 import PropTypes from 'prop-types';
+import { Col, Input, Menu, Row } from 'antd';
+
+import LoginForm from './LoginForm';
+import UserProfile from './UserProfile';
+
+const dummy = {
+  nickname: '제로초',
+  Post: [],
+  Followings: [],
+  Followers: [],
+  isLoggedIn: false,
+};
 
 const AppLayout = ({ children }) => {
   return (
@@ -13,13 +24,23 @@ const AppLayout = ({ children }) => {
           <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
         </Menu.Item>
       </Menu>
-      <Link href="/signup">
-        <a><Button>회원가입</Button></a>
-      </Link>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {dummy.isLoggedIn
+            ? <UserProfile />
+            : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="https://www.zerocho.com" target="_blank" rel="noreferrer noopener">Made by ZeroCho</a>
+        </Col>
+      </Row>
     </div>
   );
 };
+
 AppLayout.propTypes = {
   children: PropTypes.node.isRequired,
 }
--- react-nodebird-2020/front/pages/signup.js
+++ react-nodebird-2020/front/pages/signup.js
@@ -1,9 +1,19 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
-import { Button, Checkbox, Form, Input } from 'antd';
+import React, { useState, useCallback } from 'react';
+import { Form, Input, Checkbox, Button } from 'antd';
+import PropTypes from 'prop-types';
 
 import AppLayout from '../components/AppLayout';
 import useInput from '../hooks/useInput';
+
+const TextInput = ({ value }) => {
+  return (
+    <div>{value}</div>
+  )
+};
+
+TextInput.propTypes = {
+  value: PropTypes.string,
+};
 
 const Signup = () => {
   const [passwordCheck, setPasswordCheck] = useState('');
@@ -15,78 +25,65 @@ const Signup = () => {
   const [nick, onChangeNick] = useInput('');
   const [password, onChangePassword] = useInput('');
 
-  const onSubmit = () => {
+  const onSubmit = useCallback(() => {
     if (password !== passwordCheck) {
-      setPasswordError(true);
-      return;
+      return setPasswordError(true);
     }
     if (!term) {
-      setTermError(true);
-      return;
+      return setTermError(true);
     }
-    console.log({
-      id,
-      nick,
-      password,
-      passwordCheck,
-      term,
-    });
-  };
+  }, [password, passwordCheck, term]);
 
-  const onChangePasswordCheck = (e) => {
+  const onChangePasswordCheck = useCallback((e) => {
     setPasswordError(e.target.value !== password);
     setPasswordCheck(e.target.value);
-  };
+  }, [password]);
 
-  const onChangeTerm = (e) => {
+  const onChangeTerm = useCallback((e) => {
     setTermError(false);
     setTerm(e.target.checked);
-  };
+  }, []);
 
   return (
-    <>
-      <Head>
-        <title>NodeBird</title>
-      </Head>
-      <AppLayout>
-        <Form onFinish={onSubmit} style={{ padding: 10 }}>
-          <div>
-            <label htmlFor="user-id">아이디</label>
-            <br />
-            <Input name="user-id" value={id} required onChange={onChangeId} />
-          </div>
-          <div>
-            <label htmlFor="user-nick">닉네임</label>
-            <br />
-            <Input name="user-nick" value={nick} required onChange={onChangeNick} />
-          </div>
-          <div>
-            <label htmlFor="user-password">비밀번호</label>
-            <br />
-            <Input name="user-password" type="password" value={password} required onChange={onChangePassword} />
-          </div>
-          <div>
-            <label htmlFor="user-password-check">비밀번호체크</label>
-            <br />
-            <Input
-              name="user-password-check"
-              type="password"
-              value={passwordCheck}
-              required
-              onChange={onChangePasswordCheck}
-            />
-            {passwordError && <div style={{ color: 'red' }}>비밀번호가 일치하지 않습니다.</div>}
-          </div>
-          <div>
-            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>제로초 말을 잘 들을 것을 동의합니다.</Checkbox>
-            {termError && <div style={{ color: 'red' }}>약관에 동의하셔야 합니다.</div>}
-          </div>
-          <div style={{ marginTop: 10 }}>
-            <Button type="primary" htmlType="submit">가입하기</Button>
-          </div>
-        </Form>
-      </AppLayout>
-    </>
+    <AppLayout>
+      <Form onFinish={onSubmit} style={{ padding: 10 }}>
+        <TextInput value="135135" />
+        <div>
+          <label htmlFor="user-id">아이디</label>
+          <br />
+          <Input name="user-id" value={id} required onChange={onChangeId} />
+        </div>
+        <div>
+          <label htmlFor="user-nick">닉네임</label>
+          <br />
+          <Input name="user-nick" value={nick} required onChange={onChangeNick} />
+        </div>
+        <div>
+          <label htmlFor="user-password">비밀번호</label>
+          <br />
+          <Input name="user-password" type="password" value={password} required onChange={onChangePassword} />
+        </div>
+        <div>
+          <label htmlFor="user-password-check">비밀번호체크</label>
+          <br />
+          <Input
+            name="user-password-check"
+            type="password"
+            value={passwordCheck}
+            required
+            onChange={onChangePasswordCheck}
+          />
+          {passwordError && <div style={{ color: 'red' }}>비밀번호가 일치하지 않습니다.</div>}
+        </div>
+        <div>
+          <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>제로초 말을 잘 들을 것을 동의합니다.</Checkbox>
+          {termError && <div style={{ color: 'red' }}>약관에 동의하셔야 합니다.</div>}
+        </div>
+        <div style={{ marginTop: 10 }}>
+          <Button type="primary" htmlType="submit">가입하기</Button>
+        </div>
+      </Form>
+    </AppLayout>
   );
 };
 
