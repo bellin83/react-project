b85d4c27f0005bbe32093c35544b1240b506d8b1
Austin.MacbookPro <bellin83@gmail.com>
2021-04-22 05:10:35 +0900

    :bento: Add ch3 files

--- react-nodebird-2020/front/components/AppLayout.js
+++ react-nodebird-2020/front/components/AppLayout.js
@@ -2,21 +2,32 @@ import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import { Col, Input, Menu, Row } from 'antd';
+import { useSelector } from 'react-redux';
+import { createGlobalStyle } from 'styled-components';
 
 import LoginForm from './LoginForm';
 import UserProfile from './UserProfile';
 
-const dummy = {
-  nickname: '제로초',
-  Post: [],
-  Followings: [],
-  Followers: [],
-  isLoggedIn: false,
-};
+const Global = createGlobalStyle`
+  .ant-row {
+    margin-right: 0 !important;
+    margin-left: 0 !important;
+  }
+  
+  .ant-col:first-child {
+      padding-left: 0 !important;
+  }
+  
+  .ant-col:last-child {
+    padding-right: 0 !important;
+  }
+`;
 
 const AppLayout = ({ children }) => {
+  const { isLoggedIn } = useSelector(state => state.user);
   return (
     <div>
+      <Global />
       <Menu mode="horizontal">
         <Menu.Item key="home"><Link href="/"><a>노드버드</a></Link></Menu.Item>
         <Menu.Item key="profile"><Link href="/profile"><a>프로필</a></Link></Menu.Item>
@@ -26,7 +37,7 @@ const AppLayout = ({ children }) => {
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          {dummy.isLoggedIn
+          {isLoggedIn
             ? <UserProfile />
             : <LoginForm />}
         </Col>
@@ -43,6 +54,6 @@ const AppLayout = ({ children }) => {
 
 AppLayout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
 export default AppLayout;
--- react-nodebird-2020/front/components/LoginForm.js
+++ react-nodebird-2020/front/components/LoginForm.js
@@ -1,16 +1,21 @@
 import React, { useCallback } from 'react';
 import { Button, Form, Input } from 'antd';
 import Link from 'next/link';
+import { useDispatch } from 'react-redux';
 
 import useInput from '../hooks/useInput';
+import { loginAction } from '../reducers/user';
 
 const LoginForm = () => {
   const [id, onChangeId] = useInput('');
   const [password, onChangePassword] = useInput('');
+  const dispatch = useDispatch();
+
   const onSubmitForm = useCallback(() => {
-    console.log({
-      id, password,
-    });
+    dispatch(loginAction({
+      id,
+      password,
+    }));
   }, [id, password]);
 
   return (
