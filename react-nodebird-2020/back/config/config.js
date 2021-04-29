b8d1bb416b7d236b7fa2a947b7a626966fdbcd11
bellin83 <bellin83@gmail.com>
2021-04-29 14:40:58 +0900

    :art: Update version

--- react-nodebird-2020/back/config/config.js
+++ react-nodebird-2020/back/config/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 module.exports = {
   development: {
     username: "root",
-    password: process.env.DB_PASSWORD,
+    password: "whddls83",
     database: "react-nodebird",
     host: "127.0.0.1",
     port: "13306",
